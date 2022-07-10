8caba3c235abc04b8eda90a4f50ab294293d40f5
michaelskyba <michaelskyba1411@gmail.com>
2022-07-10 19:02:42 -0400

    creds.go: get pr output filename

--- mgt/creds.go
+++ mgt/creds.go
@@ -4,7 +4,31 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"strings"
 )
+
+// Handle errors
+func hdl(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Returns the filename (including path) of the pr script we're making
+func getFilename() string {
+	// e.g. "/home/michael/src/rets/mgt/creds"
+	executable, err := os.Executable()
+	hdl(err)
+
+	// e.g. []string{"home", "michael", ..., "mgt", "creds"}
+	path := strings.Split(executable, "/")
+
+	// Get rid of the last two items to get the root directory
+	root := path[0 : len(path)-2]
+
+	return strings.Join(append(root, "pr"), "/")
+
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -19,6 +43,8 @@ func main() {
 	fmt.Printf("Enter your RETS password.\n> ")
 	scanner.Scan()
 	password := scanner.Text()
+	filename := getFilename()
 
 	fmt.Println(username, password)
+	fmt.Println(filename)
 }
