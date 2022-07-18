aff71b12deb8089c4694e53181aa9358a40d14ee
michaelskyba <michaelskyba1411@gmail.com>
2022-07-18 13:51:13 -0400

    add gen_fields code

---
+++ util/gen_fields/gen.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"os"
+	"bufio"
+)
+
+func hdl(err error, message string) {
+	if err != nil {
+		uError(message)
+	}
+}
+
+func uError(message string) {
+	fmt.Fprintln(os.Stderr, message)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		uError("Invalid usage. See the gen_fields README.md.")
+	}
+
+	inputFile, err := os.Open(os.Args[1])
+	defer inputFile.Close()
+	hdl(err, "Error: Failed to open input file.")
+
+	scanner := bufio.NewScanner(inputFile)
+
+	scanner.Scan()
+	columns := strings.Split(scanner.Text(), "\t")
+
+	scanner.Scan()
+	values := strings.Split(scanner.Text(), "\t")
+
+	output := []string{}
+
+	for i, _ := range columns {
+		column := columns[i]
+		value := values[i]
+
+		if value == "" {
+			value = "gen.go: None"
+		}
+
+		output = append(output, "\n-- Column --------- :", column,
+		                        "-- Value ---------- :", value)
+	}
+
+	data := []byte(strings.Join(output, "\n"))
+	err = os.WriteFile(os.Args[2], data, 0644)
+	hdl(err, "Error: Couldn't write to output file")
+}
