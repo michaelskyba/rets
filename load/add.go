338b5576f7349068a473fc810d5912a1e991c8c6
Michael Skyba <michaelskyba@net-c.com>
2022-08-04 14:50:23 -0400

    create string --> string map from xml file lines

--- load/add.go
+++ load/add.go
@@ -8,6 +8,11 @@ import (
 	"strings"
 )
 
+type Columns struct {
+	names     []string
+	completed bool
+}
+
 func addCommand(db *sql.DB, args []string) {
 	if len(args) != 5 {
 		usageError()
@@ -23,13 +28,49 @@ func addCommand(db *sql.DB, args []string) {
 	scanner := bufio.NewScanner(file)
 	hdl(err)
 
+	columns := Columns{
+		names:     []string{},
+		completed: false,
+	}
+
+	// Iterate over xml file lines, adding each to the provided table
 	for scanner.Scan() {
-		fields := strings.Split(scanner.Text(), "	")
+		values := strings.Split(scanner.Text(), "	")
 
 		// Skip the first two rows and the very last row, which aren't normal
 		// data entries
-		if len(fields) == 1 {
-			fmt.Println(fields)
+		if len(values) == 1 {
+			continue
 		}
+
+		record := map[string]string{}
+		last := len(values) - 1
+
+		for idx, value := range values {
+			// We don't care about the <DATA> and </DATA> columns
+			if idx == 0 || idx == last {
+				continue
+			}
+
+			// We are looking at the field names declaration row (which should
+			// be the third line in the file), so we need to keep special track of
+			// them
+			if !columns.completed {
+				columns.names = append(columns.names, value)
+
+			} else {
+				// We skipped the first column ("<DATA>") and are now one ahaed
+				columnIndex := idx - 1
+				record[columns.names[columnIndex]] = value
+			}
+		}
+
+		// If we got this far, we must have just compiled the column names in
+		// the values iteration
+		if !columns.completed {
+			columns.completed = true
+		}
+
+		fmt.Println(record)
 	}
 }
