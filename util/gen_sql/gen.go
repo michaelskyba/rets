f29aef91a8134ccb6982a9c698200bccf1abcae2
Michael Skyba <michaelskyba@net-c.com>
2022-08-06 21:18:48 -0400

    gen_sql: Get access to input xml

--- util/gen_sql/gen.go
+++ util/gen_sql/gen.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
+
+func hdl(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func printError(message string) {
 	fmt.Fprintln(os.Stderr, message)
@@ -12,5 +19,14 @@ func printError(message string) {
 func main() {
 	if len(os.Args) != 3 {
 		printError("Invalid usage. See gen_sql/README.md.")
+	}
+
+	file, err := os.Open(os.Args[1])
+	hdl(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 }
