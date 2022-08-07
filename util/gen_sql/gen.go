a945ed66e418910f641f5a841a1f9248b20877a4
Michael Skyba <michaelskyba@net-c.com>
2022-08-06 21:26:22 -0400

    gen_sql: get column indices from metadata

--- util/gen_sql/gen.go
+++ util/gen_sql/gen.go
@@ -4,7 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type Indices struct {
+	SystemName    int
+	MaximumLength int
+	DataType      int
+}
 
 func hdl(err error) {
 	if err != nil {
@@ -13,12 +20,35 @@ func hdl(err error) {
 }
 
 func printError(message string) {
-	fmt.Fprintln(os.Stderr, message)
+	fmt.Fprintln(os.Stderr, message+" See gen_sql/README.md.")
+}
+
+func getIndices(input string) Indices {
+	arr := [3]int{-1, -1, -1}
+	columns := strings.Split(input, "	")
+
+	for i, column := range columns {
+		if column == "SystemName" {
+			arr[0] = i
+
+		} else if column == "MaximumLength" {
+			arr[1] = i
+
+		} else if column == "DataType" {
+			arr[2] = i
+		}
+	}
+
+	if arr[0] == -1 || arr[1] == -1 || arr[2] == -1 {
+		printError("Invalid metadata input.")
+	}
+
+	return Indices{arr[0], arr[1], arr[2]}
 }
 
 func main() {
 	if len(os.Args) != 3 {
-		printError("Invalid usage. See gen_sql/README.md.")
+		printError("Invalid usage.")
 	}
 
 	file, err := os.Open(os.Args[1])
@@ -26,7 +56,6 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	scanner.Scan()
+	columnIndices := getIndices(scanner.Text())
 }
